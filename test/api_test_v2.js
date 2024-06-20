57f5138099b752425c1d3f94eef4914480638eda
gabrielardian <gabrielstoner27@gmail.com>
2024-06-20 15:06:23 +0700

    test supertest

--- test/api_test_v2.js
+++ test/api_test_v2.js
@@ -7,15 +7,3 @@
         // test logic
     // });
 // });
-
-    // it("T2 - Post Create ", async () => {
-    //     const requestData = {
-    //         "name": "morpheus",
-    //         "job": "leader"
-    //     };
-    //     const schema = JSON.parse(fs.readFileSync("resource/schema/post_create.json",'utf8'));
-    //     const response = await supertest(baseURL)
-    //     .put("/users")
-    //     .send(requestData)
-    //     expect(response.body).to.be.jsonSchema(requestData);
-    // });
